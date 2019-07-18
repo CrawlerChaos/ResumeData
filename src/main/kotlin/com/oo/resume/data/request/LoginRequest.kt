e1381e2c31e37eb5d61085e15e87d70aa8f43a98
yangchao29 <yangchao29@meituan.com>
2019-07-18 20:29:50 +0800

    init data

---
+++ src/main/kotlin/com/oo/resume/data/request/LoginRequest.kt
@@ -0,0 +1,12 @@
+package com.oo.resume.data.request
+
+/**
+ *   yangchao
+ *    cd.uestc.superyoung@gmail.com
+ *     2019-06-06 17:01
+ *
+ */
+data class LoginRequest constructor(
+        val phone: String?,
+        val password: String?
+)
